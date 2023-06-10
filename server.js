15b382bd9532c639f61b4e4fe5e219face56609c
Kevin Li <kevinli617@gmail.com>
2023-06-09 23:13:53 -0400

    boilerplate for react app

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require("./db");
 const scraper = require("./app/scraper");
 const sendMail = require("./app/nodemailer");
 const alerter = require("./app/alerter");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +15,14 @@ app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/api", Router);
-app.use("*", express.static("client"));
+
+// Serve static files from the 'build' directory
+app.use(express.static("build"));
+
+// Handle all other requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`Application is listening on port ${PORT}.`)
