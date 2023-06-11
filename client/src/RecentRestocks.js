114d6c674ba37c4194d05fc47dee341ea9e09664
Kevin Li <kevinli617@gmail.com>
2023-06-11 01:06:08 -0400

    made restocks page responsie

--- client/src/RecentRestocks.js
+++ client/src/RecentRestocks.js
@@ -2,36 +2,42 @@ import image1 from "./images/restock_1.jpg";
 
 const RecentRestocks = () => {
   return (
-    <div className="container bg-light mt-4 px-5 py-3 rounded w-100">
-      <div className="d-flex justify-content-center">
-        <div className="card mx-4" style={{ width: "18rem" }}>
-          <img src={image1} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Space Malice Bag</h5>
-            <p className="card-text">Price: $22,700</p>
-            <a href="#" className="btn btn-primary">
-              View Details
-            </a>
+    <div className="container bg-light mt-4 px-3 py-3 rounded">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <div className="card mb-4" style={{ width: "100%" }}>
+            <img src={image1} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Space Malice Bag</h5>
+              <p className="card-text">Price: $22,700</p>
+              <a href="#" className="btn btn-primary">
+                View Details
+              </a>
+            </div>
           </div>
         </div>
-        <div className="card mx-4" style={{ width: "18rem" }}>
-          <img src={image1} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Space Malice Bag</h5>
-            <p className="card-text">Price: $22,700</p>
-            <a href="#" className="btn btn-primary">
-              View Details
-            </a>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <div className="card mb-4" style={{ width: "100%" }}>
+            <img src={image1} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Space Malice Bag</h5>
+              <p className="card-text">Price: $22,700</p>
+              <a href="#" className="btn btn-primary">
+                View Details
+              </a>
+            </div>
           </div>
         </div>
-        <div className="card mx-4" style={{ width: "18rem" }}>
-          <img src={image1} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Space Malice Bag</h5>
-            <p className="card-text">Price: $22,700</p>
-            <a href="#" className="btn btn-primary">
-              View Details
-            </a>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <div className="card mb-4" style={{ width: "100%" }}>
+            <img src={image1} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Space Malice Bag</h5>
+              <p className="card-text">Price: $22,700</p>
+              <a href="#" className="btn btn-primary">
+                View Details
+              </a>
+            </div>
           </div>
         </div>
       </div>
