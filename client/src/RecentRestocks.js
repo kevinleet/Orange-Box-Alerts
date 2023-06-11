0108dc026f69221aeab4026392a52ce39e4b62da
Kevin Li <kevinli617@gmail.com>
2023-06-11 00:57:26 -0400

    centered elements on restocks page

--- client/src/RecentRestocks.js
+++ client/src/RecentRestocks.js
@@ -1,42 +1,35 @@
-// import { useState } from "react";
 import image1 from "./images/restock_1.jpg";
-
-// const [restockItems, setRestockItems] = useState(getRestockItems())
-
-// async function getRestockItems() {
-// let response =
-// }
 
 const RecentRestocks = () => {
   return (
     <div className="container bg-light mt-4 px-5 py-3 rounded w-100">
-      <div className="d-inline-flex flex-row ">
-        <div class="card" style={{ width: "18rem" }}>
-          <img src={image1} class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Space Malice Bag</h5>
-            <p class="card-text">Price: $22,700</p>
-            <a href="#" class="btn btn-primary">
+      <div className="d-flex justify-content-center">
+        <div className="card mx-4" style={{ width: "18rem" }}>
+          <img src={image1} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Space Malice Bag</h5>
+            <p className="card-text">Price: $22,700</p>
+            <a href="#" className="btn btn-primary">
               View Details
             </a>
           </div>
         </div>
-        <div class="card" style={{ width: "18rem" }}>
-          <img src={image1} class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Space Malice Bag</h5>
-            <p class="card-text">Price: $22,700</p>
-            <a href="#" class="btn btn-primary">
+        <div className="card mx-4" style={{ width: "18rem" }}>
+          <img src={image1} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Space Malice Bag</h5>
+            <p className="card-text">Price: $22,700</p>
+            <a href="#" className="btn btn-primary">
               View Details
             </a>
           </div>
         </div>
-        <div class="card" style={{ width: "18rem" }}>
-          <img src={image1} class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Space Malice Bag</h5>
-            <p class="card-text">Price: $22,700</p>
-            <a href="#" class="btn btn-primary">
+        <div className="card mx-4" style={{ width: "18rem" }}>
+          <img src={image1} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Space Malice Bag</h5>
+            <p className="card-text">Price: $22,700</p>
+            <a href="#" className="btn btn-primary">
               View Details
             </a>
           </div>
