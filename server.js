9174551857881a828d466e791adf83ad9b1e7e22
Kevin Li <kevinli617@gmail.com>
2023-06-11 17:10:44 -0400

    updated server.js to redirect to correct directory

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "client/dist")));
 
 // Handle all other requests
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 app.listen(PORT, () =>
