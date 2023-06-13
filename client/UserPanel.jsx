d4380dd4f94cf3d3f2a6baa1633a6bd4f1aac683
Kevin Li <kevinli617@gmail.com>
2023-06-13 11:01:14 -0400

    added individual products to alert mapping for notifications page

--- client/UserPanel.jsx
+++ client/UserPanel.jsx
@@ -1,13 +1,14 @@
-import { Container, Button, Tab, Tabs, ListGroup } from "react-bootstrap";
+import { Container, Button, Tab, Tabs, ListGroup, Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const UserPanel = ({ userData, setUserData, isLoggedIn, setIsLoggedIn }) => {
+  const [products, setProducts] = useState([]);
   const [dbData, setDbData] = useState({
     subscription_active: "",
     notify_all_restocks: "",
-    products_to_alert: "",
+    products_to_alert: [],
   });
   const navigate = useNavigate();
   useEffect(() => {
@@ -27,7 +28,7 @@ const UserPanel = ({ userData, setUserData, isLoggedIn, setIsLoggedIn }) => {
     setDbData({
       subscription_active: "",
       notify_all_restocks: "",
-      products_to_alert: "",
+      products_to_alert: [],
     });
     setIsLoggedIn(false);
     localStorage.setItem("isLoggedIn", "false");
@@ -36,12 +37,59 @@ const UserPanel = ({ userData, setUserData, isLoggedIn, setIsLoggedIn }) => {
     let response = await axios.get(`api/users?email=${userData.email}`);
     const { subscription_active, notify_all_restocks, products_to_alert } =
       response.data[0];
-    setDbData({
-      subscription_active: subscription_active,
-      notify_all_restocks: notify_all_restocks,
-      products_to_alert: products_to_alert,
-    });
+    if (!products_to_alert) {
+      setDbData({
+        subscription_active: subscription_active,
+        notify_all_restocks: notify_all_restocks,
+        products_to_alert: [],
+      });
+    } else {
+      setDbData({
+        subscription_active: subscription_active,
+        notify_all_restocks: notify_all_restocks,
+        products_to_alert: products_to_alert,
+      });
+    }
   }
+
+  async function enableAllRestocks() {
+    try {
+      console.log("enabling all restocks");
+      await axios.put(`api/users/${userData.email}`, {
+        notify_all_restocks: "true",
+      });
+      setDbData({ notify_all_restocks: "true" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function disableAllRestocks() {
+    try {
+      console.log("disabling all restocks");
+      await axios.put(`api/users/${userData.email}`, {
+        notify_all_restocks: "false",
+      });
+      setDbData({ notify_all_restocks: "false" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function enableProductAlert() {}
+
+  async function disableProductAlert() {}
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        let response = await axios.get("/api/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
   return (
     <Container className="bg-light mt-md-3 py-3 rounded w-100 ">
       <Tabs
@@ -66,6 +114,7 @@ const UserPanel = ({ userData, setUserData, isLoggedIn, setIsLoggedIn }) => {
               never miss out on any important updates!
             </p>
           </Container>
+
           <Container className="d-flex justify-content-center mt-5">
             <stripe-buy-button
               buy-button-id="buy_btn_1NIIfAEnnrWrEekPzBiz7bGy"
@@ -76,9 +125,67 @@ const UserPanel = ({ userData, setUserData, isLoggedIn, setIsLoggedIn }) => {
         <Tab
           eventKey="Notification Settings"
           title="Notification Settings"
-          disabled
+          // disabled
         >
-          Tab content for Alerts
+          <Container>
+            <Container className="mt-4">
+              <Card style={{ width: "20rem" }}>
+                <Card.Body>
+                  <Card.Title>
+                    Notify All Restocks{" "}
+                    {dbData.notify_all_restocks == "false" ? (
+                      <span style={{ color: "red" }}>(Disabled)</span>
+                    ) : (
+                      <span style={{ color: "green" }}>(Enabled)</span>
+                    )}
+                  </Card.Title>
+                  <Card.Text>
+                    Enable instant email notifications for all product restocks!
+                  </Card.Text>
+                  {dbData.notify_all_restocks == "false" ? (
+                    <Button variant="secondary" onClick={enableAllRestocks}>
+                      Enable
+                    </Button>
+                  ) : (
+                    <Button variant="success" onClick={disableAllRestocks}>
+                      Disable
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            </Container>
+            <hr />
+
+            <Container>
+              {products.map((item) => (
+                <Card style={{ width: "20rem" }}>
+                  <Card.Body>
+                    <Card.Title>
+                      {item.name}{" "}
+                      {dbData.notify_all_restocks.includes(item.name) ? (
+                        <span style={{ color: "green" }}>(Enabled)</span>
+                      ) : (
+                        <span style={{ color: "red" }}>(Disabled)</span>
+                      )}
+                    </Card.Title>
+                    <Card.Text>
+                      Enable instant email notifications for all{" "}
+                      <strong>{item.name}</strong> restocks!
+                    </Card.Text>
+                    {dbData.notify_all_restocks.includes(item.name) ? (
+                      <Button variant="success" onClick={disableProductAlert}>
+                        Disable
+                      </Button>
+                    ) : (
+                      <Button variant="secondary" onClick={enableProductAlert}>
+                        Enable
+                      </Button>
+                    )}
+                  </Card.Body>
+                </Card>
+              ))}
+            </Container>
+          </Container>
         </Tab>
         <Tab eventKey="profile" title="Profile">
           <Container>
