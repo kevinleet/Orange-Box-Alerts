86f9589b2323db23c3062f5b92a85ca83853d32b
Kevin Li <kevinli617@gmail.com>
2023-07-13 12:24:43 -0400

    updated footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -9,15 +9,18 @@ const Footer = () => {
         className="small text-body-secondary small-text"
         style={{ marginBottom: "0" }}
       >
-        © Orange Box Alerts 2023 | Developed By Kevin Li{" "}
+        © Orange Box Alerts 2023 | Developed By{" "}
+        <a href="https://kevinli.boston/" target="_blank">
+          Kevin Li
+        </a>{" "}
       </p>
-      <a target="_blank" href="https://github.com/kevinleet">
+      {/* <a target="_blank" href="https://github.com/kevinleet">
         <img src={github} style={{ maxHeight: "20px" }} />
       </a>
       {"   "}
       <a target="_blank" href="https://www.linkedin.com/in/kevinli617/">
         <img src={linkedin} style={{ maxHeight: "15px" }} />
-      </a>
+      </a> */}
     </Container>
   );
 };
